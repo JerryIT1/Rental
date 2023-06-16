2542b724a0c4f7c793778ec8ab66338ea1084c05
JollyPart <94416528+JerryIT1@users.noreply.github.com>
2023-06-15 22:25:20 -0500

    Information
    Got more work done on information page / added assets

--- src/Components/Information/Information.jsx
+++ src/Components/Information/Information.jsx
@@ -1,9 +1,34 @@
 import React from "react"
 import './information.css'
+import car from './Assets/select.png'
+import support from './Assets/support.png'
+import weather from './Assets/weather.png'
+
+const Info = (props) =>{
+    return(
+        <div className="perks-singles">
+            <img src={props.img} className="perks-image"/>
+            <p>{props.desc}</p>
+        </div>
+    )
+}
 
 const Information = () => {
     return(
-        <div>Info</div>
+        <div className="info__container">
+            <div className="info__container-header">
+                Plan your trip now
+            </div>
+            <div className="info__container-brand">
+                Quick & easy car rental
+            </div>
+            <div className="info__container-perks">
+                <Info 
+                img = {car}
+                desc = {"How are you"}
+                />
+            </div>
+        </div>
     )
 }
 
